55886d953bbe6ee5a49571fed4fb8ad443984ef9
Ghomsi Bopda <hardy.ghomsibopda@cgi.com>
2024-04-05 15:16:30 -0400

    first commit

---
+++ MAPPING/MODEL/User.cs
@@ -0,0 +1,16 @@
+﻿namespace MAPPING.MODEL
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public string MobileNumber { get; set; }
+        public string Sex { get; set; }
+    }
+}
---
+++ MAPPING/PROFILE/UserProfile.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MAPPING.DTO;
+using MAPPING.MODEL;
+
+namespace MAPPING.PROFILE
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile() 
+        {
+            CreateMap<UserDto, User>()
+                .ForMember(
+                    dest => dest.FirstName,
+                    opt => opt.MapFrom(src => $"{src.FirstName}")
+                )
+                .ForMember(
+                    dest => dest.LastName,
+                    opt => opt.MapFrom(src => $"{src.LastName}")
+                )
+                .ForMember(
+                    dest => dest.Email,
+                    opt => opt.MapFrom(src => $"{src.Email}")
+                )
+                .ForMember(
+                    dest => Convert.ToDateTime(dest.DateOfBirth),
+                    opt => opt.MapFrom(src => $"{src.DateOfBirth}")
+                )
+                .ForMember(
+                    dest => dest.Phone,
+                    opt => opt.MapFrom(src => $"{src.Phone}")
+                )
+                .ForMember(
+                    dest => dest.Country,
+                    opt => opt.MapFrom(src => $"{src.Country}")
+                );
+        }
+    }
+}
